3a4ef6344dab6a7ddacb90bf16c3a071f280f373
Khizer Younas <m.khizeryounas@gmail.com>
2020-01-09 23:18:36 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,9 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         TZ: 'UTC',
-        PORT: 8080
+        PORT: 8080,
+        secret: '92cc13d9-fb39-46d5-a428-6698f4a8b5bd',
+        api_key: '5e34d2a6-a11a-4656-948c-cd2840661f0d',
       }
     }
   ]
